7988fa9fc97250f97fbe689b978aae3cb7cfa183
Ariel Maldonado <arielsebas2000@gmail.com>
2022-09-11 13:57:55 -0500

    Primera Funcion en controller - 11/09/2022

--- src/main/java/com/uce/edu/demo/controller/VehiculoController.java
+++ src/main/java/com/uce/edu/demo/controller/VehiculoController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.repository.modelo.Vehiculo;
@@ -15,25 +14,24 @@ import com.uce.edu.demo.service.IVehiculoService;
 @Controller
 @RequestMapping("/vehiculos")
 public class VehiculoController {
-	
+
 	@Autowired
 	private IVehiculoService vehiculoService;
-	
-	
-	@GetMapping("/buscarDisponibles/{marca}{modelo}")
-	public String vehiculosDisponibles(@PathVariable("marca")String marca,@PathVariable("modelo")String modelo, Model model) {
-		System.out.println(marca+"---------- "+modelo);
-		List<Vehiculo>disponibles=this.vehiculoService.buscarMarcaModeloDisponible(marca, modelo);
+
+	@GetMapping("/buscarDisponibles")
+	public String vehiculosDisponibles(Vehiculo vehiculo, Model model) {
+		System.out.println(vehiculo.getMarca() + " ---------- " + vehiculo.getModelo());
+		List<Vehiculo> disponibles = this.vehiculoService.buscarMarcaModeloDisponible(vehiculo.getMarca(),
+				vehiculo.getModelo());
 		model.addAttribute("disponibles", disponibles);
 		return "vistaBusquedaDisponibles";
 	}
-	
+
+	// Metodos de redireccionamiento a paginas
 	@GetMapping("/disponibles")
 	public String consultaDisponibles(Vehiculo vehiculo) {
-	
-		System.out.println("-------"+vehiculo.getMarca()+" "+vehiculo.getModelo());
+		System.out.println("------- " + vehiculo.getMarca() + " " + vehiculo.getModelo());
 		return "vistaConsultaDisponibles";
 	}
-	
-	
+
 }
