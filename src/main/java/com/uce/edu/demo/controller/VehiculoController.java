09fef8da9663243e6649690e31f1e96865c78410
ec98 <carlospiruch98@gmail.com>
2022-09-12 01:38:57 -0500

    Pruebas Unitarias y Retirar un vehiculo sin reserva

--- src/main/java/com/uce/edu/demo/controller/VehiculoController.java
+++ src/main/java/com/uce/edu/demo/controller/VehiculoController.java
@@ -9,10 +9,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.repository.modelo.Reserva;
 import com.uce.edu.demo.repository.modelo.Vehiculo;
+import com.uce.edu.demo.repository.modelo.dto.ReporteVIP;
 import com.uce.edu.demo.repository.modelo.dto.controller.ReservaDto;
 import com.uce.edu.demo.service.IGestorReservasService;
 import com.uce.edu.demo.service.IReservaService;
@@ -28,12 +30,12 @@ public class VehiculoController {
 	private IGestorReservasService gestorReservasService;
 	@Autowired
 	private IReservaService reservaService;
-	
+
 	@GetMapping("/disponibles")
 	public String consultaDisponibles(Vehiculo vehiculo) {
 		return "vistaConsultaDisponibles";
 	}
-	
+
 	@GetMapping("/buscarDisponibles")
 	public String vehiculosDisponibles(Vehiculo vehiculo, Model model) {
 		List<Vehiculo> disponibles = this.vehiculoService.buscarMarcaModeloDisponible(vehiculo.getMarca(),
@@ -41,12 +43,12 @@ public class VehiculoController {
 		model.addAttribute("disponibles", disponibles);
 		return "vistaBusquedaDisponibles";
 	}
-	
+
 	@GetMapping("/crearReserva")
 	public String paginaNuevoProducto(ReservaDto reservaDto) {
 		return "vistaGenerarReserva";
 	}
-	
+
 	@PostMapping("/reservar")
 	public String reservarVehiculo(ReservaDto reserva, Model modelo) {
 
@@ -58,6 +60,26 @@ public class VehiculoController {
 		return "vistaConfirmacion";
 	}
 
+	@PutMapping("/noReservados")
+	public String sinReservarVehiculo(ReporteVIP reporte) {
+
+		List<Vehiculo> vehiculosD = this.vehiculoService.buscarMarcaModeloDisponible(reporte.getMarca(),
+				reporte.getModelo());
+		Vehiculo v = this.vehiculoService.buscarPlaca(reporte.getPlaca());
+		if (vehiculosD.contains(v)) {
+			String numero = this.gestorReservasService.reservarVehiculo(reporte.getPlaca(), reporte.getCedula(),
+					LocalDateTime.now(), LocalDateTime.parse(reporte.getFechaFin()));
+			this.gestorReservasService.retirarVehiculo(numero);
+		}
+		return "redirect:/vehiculos/disponibles";
+	}
+
+	@GetMapping("/vistaNoReservados")
+	public String vistaNoReservados(ReporteVIP reporte, Model modelo) {
+		modelo.addAttribute("reporte", reporte);
+		return "vistaNoReservados";
+	}
+
 	@GetMapping("/confirmacionReserva/{numero}")
 	public String confirmacionReserva(@PathVariable("numero") String numero, Model modelo) {
 		System.out.println(numero);
@@ -65,7 +87,5 @@ public class VehiculoController {
 		modelo.addAttribute("verificacion", verificacion);
 		return "redirect:/vehiculos/crearReserva";
 	}
-
-	
 
 }
---
+++ src/test/java/com/uce/edu/demo/service/test/IReservaServiceTest.java
@@ -0,0 +1,128 @@
+package com.uce.edu.demo.service.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.uce.edu.demo.repository.modelo.Reserva;
+import com.uce.edu.demo.repository.modelo.Vehiculo;
+import com.uce.edu.demo.service.IReservaService;
+import com.uce.edu.demo.service.IVehiculoService;
+
+@SpringBootTest
+@Transactional
+@Rollback(true)
+class IReservaServiceTest {
+
+	@Autowired
+	private IReservaService iReservaService;
+
+	@Autowired
+	private IVehiculoService iVehiculoService;
+
+	@Test
+	void testInsertar() {
+		Vehiculo v = new Vehiculo();
+		v.setPlaca("ASD-333");
+		v.setMarca("Audi");
+		v.setModelo("Blackc");
+		v.setAvaluo(new BigDecimal(20000));
+		v.setAnioFabricacion("2021");
+		v.setCilindraje("18CV");
+		v.setPaisFabricacion("Francia");
+		v.setValorPorDia(new BigDecimal(200));
+		v.setEstado("D");
+
+		this.iVehiculoService.insertar(v);
+
+		Reserva reserva = new Reserva();
+
+		reserva.setNumero("0001");
+		reserva.setEstado("D");
+		reserva.setFechaInicio(LocalDateTime.of(2022, 9, 11, 12, 30));
+		reserva.setFechaFin(LocalDateTime.of(2022, 9, 13, 12, 30));
+		reserva.setIva(new BigDecimal(12));
+		reserva.setSubTotal(new BigDecimal(150));
+		reserva.setTotalPagar(new BigDecimal(1500));
+		reserva.setVehiculo(v);
+
+		this.iReservaService.insertar(reserva);
+		assertThat(this.iReservaService.buscarNumero(reserva.getNumero()).getNumero()).isNotNull();
+
+	}
+
+	@Test
+	void testBuscarNumero() {
+		Vehiculo v = new Vehiculo();
+		v.setPlaca("ASD-333");
+		v.setMarca("Audi");
+		v.setModelo("Blackc");
+		v.setAvaluo(new BigDecimal(20000));
+		v.setAnioFabricacion("2021");
+		v.setCilindraje("18CV");
+		v.setPaisFabricacion("Francia");
+		v.setValorPorDia(new BigDecimal(200));
+		v.setEstado("D");
+
+		this.iVehiculoService.insertar(v);
+
+		Reserva reserva = new Reserva();
+		reserva.setNumero("0001");
+		reserva.setEstado("D");
+		reserva.setFechaInicio(LocalDateTime.of(2022, 9, 11, 12, 30));
+		reserva.setFechaFin(LocalDateTime.of(2022, 9, 13, 12, 30));
+		reserva.setIva(new BigDecimal(12));
+		reserva.setSubTotal(new BigDecimal(150));
+		reserva.setTotalPagar(new BigDecimal(1500));
+		reserva.setVehiculo(v);
+
+		this.iReservaService.insertar(reserva);
+
+		Reserva reservaBuscado = this.iReservaService.buscarNumero(reserva.getNumero());
+		assertEquals(reserva, reservaBuscado);
+	}
+
+	@Test
+	void testActualizar() {
+		Vehiculo v = new Vehiculo();
+		v.setPlaca("ASD-333");
+		v.setMarca("Audi");
+		v.setModelo("Blackc");
+		v.setAvaluo(new BigDecimal(20000));
+		v.setAnioFabricacion("2021");
+		v.setCilindraje("18CV");
+		v.setPaisFabricacion("Francia");
+		v.setValorPorDia(new BigDecimal(200));
+		v.setEstado("D");
+
+		this.iVehiculoService.insertar(v);
+
+		Reserva reserva = new Reserva();
+		reserva.setNumero("0001");
+		reserva.setEstado("N");
+		reserva.setFechaInicio(LocalDateTime.of(2022, 9, 11, 12, 30));
+		reserva.setFechaFin(LocalDateTime.of(2022, 9, 13, 12, 30));
+		reserva.setIva(new BigDecimal(12));
+		reserva.setSubTotal(new BigDecimal(150));
+		reserva.setTotalPagar(new BigDecimal(1500));
+		reserva.setVehiculo(v);
+
+		this.iReservaService.insertar(reserva);
+
+		reserva.setNumero("0002");
+
+		this.iReservaService.actualizar(reserva);
+		assertEquals(this.iReservaService.buscarNumero(reserva.getNumero()).getNumero(), reserva.getNumero());
+
+	}
+
+}
